e955b459d312de39cc9335f658dae975b6af71f0
anna-makarchuk <amakarchuk1820@gmail.com>
2023-08-03 09:18:55 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,15 +2,9 @@ import throttle from "lodash.throttle";
 
 const form = document.querySelector('.feedback-form');
 const keyStorage = "feedback-form-state";
-
 let currentData;
-
 form.addEventListener('input', throttle(onInput,500));
- 
-
 const currentLocalData = JSON.parse(localStorage.getItem(keyStorage));
-
-
 if (currentLocalData) {
     form.email.value = currentLocalData.email;
     form.message.value = currentLocalData.message;
@@ -19,25 +13,19 @@ if (currentLocalData) {
         message:form.message.value,
     };
 };
-
-    
-function onInput(event) {
-   currentData = {
-        email:event.target.value,
-        message:event.target.value,
+function onInput() {
+    currentData = {
+        email: form.email.value,
+        message: form.message.value,
     };
     localStorage.setItem(keyStorage, JSON.stringify(currentData));
 };
-
-
 form.addEventListener('submit', onSubmit);
-
-function onSubmit() {
-    
-   console.log(currentData); 
+function onSubmit(e) {
+    e.preventDefault();
+     console.log(currentData);
     localStorage.removeItem(keyStorage);
-    reset(form);
-  
+    form.reset();
 };
 
 
